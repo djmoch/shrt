fa83b57a422b15a411d1cc9ffd63947010f60170
Daniel Moch <daniel@danielmoch.com>
2022-10-20 14:21:13 -0400

    Add headers, test files to DIST_SRC

--- config.mk
+++ config.mk
@@ -7,7 +7,19 @@ SRC = \
 	shrt.c \
 	shrtfile.c
 OBJ = ${SRC:c=o}
-DIST_SRC = ${SRC} Makefile config.mk shrt.1 shrtfile.5 README LICENSE
+HDRS = \
+	shrtfile.h \
+	config.h
+DIST_SRC = \
+	${SRC} \
+	${HDRS} \
+	test \
+	Makefile \
+	config.mk \
+	shrt.1 \
+	shrtfile.5 \
+	README \
+	LICENSE
 
 CFLAGS = -c -g -O0 -std=c99
 CPPFLAGS = -DVERSION=\"${VERSION}\" -D_BSD_SOURCE
